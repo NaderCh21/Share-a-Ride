a4fada481be184bbde90c8076ecb89d87a1f1f8c
RoniDaou <roni.r.daou@gmail.com>
2025-03-27 19:57:04 +0200

    Added login and signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,137 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+//const Img = require("../models/imgModel");
+const bcrypt = require("bcrypt");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+// login a user
+const loginUser = async (req, res) => {
+  const { universityEmail, password } = req.body;
+
+  try {
+    const user = await User.login(universityEmail, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ universityEmail, token, id: user._id });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// signup a user
+const signupUser = async (req, res) => {
+  const {
+    universityEmail,
+    password,
+    confirmPassword,
+    first_name,
+    last_name,
+    studentIdPic,
+    role,
+  } = req.body;
+
+  try {
+    const user = await User.signup(
+      universityEmail,
+      password,
+      confirmPassword,
+      first_name,
+      last_name,
+      studentIdPic,
+      role
+    );
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ universityEmail, token, id: user._id });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// const getUserInfo = async (req, res) => {
+//   const user_id = req.user._id;
+//   try {
+//     const user = await User.findById(user_id)
+//       .populate("profilePic")
+//       .populate({
+//         path: "userBlogs",
+//         options: { sort: { createdAt: -1 } },
+//         populate: {
+//           path: "image",
+//         },
+//       });
+
+//     if (!user) {
+//       return res.status(404).json("User not found");
+//     }
+
+//     return res.status(200).json(user);
+//   } catch (error) {
+//     console.error("Error fetching user info:", error.message);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
+
+// const updateInfo = async (req, res) => {
+//   const user_id = req.user._id;
+
+//   try {
+//     if (req.body.password && req.body.password !== "") {
+//       const salt = await bcrypt.genSalt(10);
+//       const hash = await bcrypt.hash(req.body.password, salt);
+//       req.body.password = hash;
+//     }
+
+//     if (req.body.profilePic && req.body.profilePic.data instanceof Buffer) {
+//       req.body.profilePic.data = req.body.profilePic.data.toString("base64");
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(user_id, req.body, {
+//       new: true,
+//     });
+
+//     if (!updatedUser) {
+//       return res.status(400).json("No user was found");
+//     }
+//     return res.status(200).json(updatedUser);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// const uploadPic = async (req, res) => {
+//   const id = req.user._id;
+//   try {
+//     const { image } = req.body;
+
+//     if (!image) {
+//       return res.status(400).json({ msg: "No image was found" });
+//     }
+
+//     let newImg = new Img({
+//       image,
+//       uploadedBy: id,
+//     });
+
+//     newImg = await newImg.save();
+//     await User.findByIdAndUpdate(req.user._id, { profilePic: newImg._id });
+//     res.json(newImg);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// };
+
+module.exports = {
+  //getUserInfo,
+  //updateInfo,
+  loginUser,
+  signupUser,
+  //uploadPic,
+};
---
+++ backend/models/userModel.js
@@ -0,0 +1,141 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  universityEmail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  univeristyName: {
+    type: String,
+    required: true,
+  },
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  campusLocation: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: Schema.Types.ObjectId,
+    ref: "Img",
+  },
+  studentIdPic: {
+    type: Schema.Types.ObjectId,
+    ref: "Img",
+    required: true,
+  },
+  vehicleIdPic: {
+    type: Schema.Types.ObjectId,
+    ref: "Img",
+  },
+  driverLicencePic: {
+    type: Schema.Types.ObjectId,
+    ref: "Img",
+  },
+});
+
+// static signup method
+userSchema.statics.signup = async function (
+  universityEmail,
+  password,
+  confirmPassword,
+  first_name,
+  last_name,
+  studentIdPic,
+  role
+) {
+  // validation
+  if (
+    !universityEmail ||
+    !password ||
+    confirmPassword ||
+    !first_name ||
+    !last_name ||
+    !studentIdPic ||
+    !role
+  ) {
+    throw Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    universityEmail,
+    password: hash,
+    confirmPassword: hash,
+    studentIdPic,
+    role,
+    first_name,
+    last_name,
+  });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (universityEmail, password) {
+  if (!universityEmail || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ universityEmail });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
