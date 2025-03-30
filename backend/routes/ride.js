b291c1edc725c842a7afd5520f90c4eb0f21eb80
RoniDaou <roni.r.daou@gmail.com>
2025-03-30 22:21:45 +0300

    ...

--- backend/routes/ride.js
+++ backend/routes/ride.js
@@ -4,22 +4,18 @@ const rideController = require("../controllers/rideController");
 const authMiddleware = require("../middlewares/requireAuth");
 
 //Both
-// Get all rides
+
 router.get("/getAllRides", rideController.getRides);
 
 //Driver
-// Add a new ride
+
 router.post("/driver/addRide", authMiddleware, rideController.addRide);
 
-// delete ride
 router.post(
   "/driver/deleteRide/:rideId",
   authMiddleware,
   rideController.deleteRide
 );
-
-// Delete a ride
-router.delete("/driver/:id", authMiddleware, rideController.deleteRide);
 
 router.get("/driver/My-rides", authMiddleware, rideController.getDriverRides);
 
